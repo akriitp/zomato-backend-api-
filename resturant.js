c94fa02988405594eeff843af5df976e06acaa68
akriitp <amitkumarranjan1204@gmail.com>
2024-10-30 23:14:36 +0530

    Create resturant.js

---
+++ resturant.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const resturantSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, " Resturant title is required"],
+    },
+    imageUrl: {
+      type: String,
+    },
+    foods: { type: Array },
+    time: {
+      type: String,
+    },
+    pickup: {
+      type: Boolean,
+      default: true,
+    },
+    delivery: {
+      type: Boolean,
+      default: true,
+    },
+    isOpen: {
+      type: Boolean,
+      default: true,
+    },
+    logoUrl: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      default: 1,
+      min: 1,
+      max: 5,
+    },
+    ratingCount: { type: String },
+    code: {
+      type: String,
+    };
+module.exports = mongoose.model("Resturant", resturantSchema);
